2ce1dc0afd7e1226bbbcf6076f81af82f4d79182
Abdallah-Salama2 <68562911+Abdallah-Salama2@users.noreply.github.com>
2024-02-29 17:41:26 +0200

    Updated APIs

--- app/DTO/Users/UserUpdateDTO.php
+++ app/DTO/Users/UserUpdateDTO.php
@@ -1,37 +1,100 @@
 <?php
+//
+//namespace App\DTO\Users;
+//
+//use Illuminate\Http\Request;
+//use Illuminate\Support\Str;
+//
+//
+//class UserUpdateDTO
+//{
+//    public static function userInfoFromRequest(Request $request): array
+//    {
+//        $data = [];
+//
+//        foreach ($request->all() as $key => $value) {
+//            $data[self::snakeToLowerCase($key)] = $value;
+//        }
+//
+//
+//        return $data;
+//    }
+//
+//    public static function cardInfoFromRequest(Request $request): array
+//    {
+//        $data = [];
+//
+//        foreach ($request->all() as $key => $value) {
+//            $data[self::snakeToLowerCase($key)] = $value;
+//        }
+//
+//        return $data;
+//    }
+//
+//    private static function snakeToLowerCase(string $key): string
+//    {
+//        return strtolower(Str::snake($key));
+//    }
+//}
+
 
 namespace App\DTO\Users;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Str;
-
 
 class UserUpdateDTO
 {
-    public static function userInfoFromRequest(Request $request): array
+
+        public static function userInfoFromRequest(Request $request): array
     {
         $data = [];
 
-        foreach ($request->all() as $key => $value) {
-            $data[self::snakeToLowerCase($key)] = $value;
+        if ($request->has('nationalId')) {
+            $data['national_id'] = $request->input('nationalId');
+        }
+
+        if ($request->has('name')) {
+            $data['name'] = $request->input('name');
+        }
+
+        if ($request->has('email')) {
+            $data['email'] = $request->input('email');
+        }
+
+        if ($request->has('password')) {
+            $data['password'] = $request->input('password');
+        }
+
+        if ($request->has('phoneNumber')) {
+            $data['phone_number'] = $request->input('phoneNumber');
+        }
+
+        if ($request->has('city')) {
+            $data['city_state'] = $request->input('city');
+        }
+
+
+
+        if ($request->has('street')) {
+            $data['street'] = $request->input('street');
+        }
+
+        if ($request->has('birthDate')) {
+            $data['birth_date'] = $request->input('birthDate');
         }
 
         return $data;
+
     }
 
     public static function cardInfoFromRequest(Request $request): array
     {
-        $data = [];
-
-        foreach ($request->all() as $key => $value) {
-            $data[self::snakeToLowerCase($key)] = $value;
-        }
-
-        return $data;
+        return $request->only([
+            'cardName',
+            'cardNumber',
+            'cardNationalId',
+            'cardPassword',
+        ]);
     }
 
-    private static function snakeToLowerCase(string $key): string
-    {
-        return strtolower(Str::snake($key));
-    }
 }
