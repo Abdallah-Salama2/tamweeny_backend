3dce2f554bbd84e014be4d7dbbab24c3b04f5f0a
Abdallah-Salama2 <68562911+Abdallah-Salama2@users.noreply.github.com>
2024-03-19 00:40:30 +0200

    Updated APIs

--- app/Models/AdminCard.php
+++ app/Models/AdminCard.php
@@ -8,7 +8,7 @@ class AdminCard extends Model
 {
     protected $fillable = [
         'admin_id', 'card_number', 'card_national_id', 'cardName', 'individuals_number',
-        'tamween_points', 'bread_points', 'name', 'gender', 'email', 'salary', 'national_id_card_and_birth_certificate', 'followers_national_id_cards_and_birth_certificates'
+        'tamween_points', 'bread_points', 'name', 'gender', 'email', 'social_status','phone_number','salary', 'national_id_card_and_birth_certificate', 'followers_national_id_cards_and_birth_certificates'
     ];
 
     public function governorAdmin()
