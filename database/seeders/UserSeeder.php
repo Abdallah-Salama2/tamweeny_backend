6fbc0846e83cfa65efc6fa409eeb06d911e3532a
Abdallah-Salama2 <Abdallah.Salama.95@h-eng.helwan.edu.eg>
2024-06-30 23:09:12 +0300

    big_update

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -9,6 +9,7 @@ use App\Models\GovernorAdmin;
 use App\Models\Store;
 use App\Models\storeOwner;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -46,17 +47,17 @@ class UserSeeder extends Seeder
 
         $user=User::Create([
 
-            'national_id' => '222',
+            'national_id' => '29006141400948',
             'name' => 'Alas',
             'email' => 'alas@gmail.com',
             'password' => static::$password ??= Hash::make('password'),
-            'phone_number' => '0222010',
-            'city_state' => '3amk State',
-            'street' => '3amk Street',
+            'phone_number' => '01020304050',
+            'city_state' => 'Nasr City, Cairo Governorate',
+            'street' => '29VG+H6M, Al Hay Al Asher',
             'birth_date' => '2000-11-21',
             'user_type'=>'Customer',
-            'latitude'=>11.20000000,
-            'longitude'=>11.20000000,
+            'latitude'=>30.043172504337285,
+            'longitude'=>31.37297504641439,
         ]);
         Card::create([
             'user_id'=>$user->id,
@@ -67,6 +68,7 @@ class UserSeeder extends Seeder
         ]);
 
         $user->assignRole('customer');
+        $currentTimestamp = Carbon::now();
 
         $supplier = User::create([
             'email' => 'Supplier@gmail.com',
@@ -74,21 +76,25 @@ class UserSeeder extends Seeder
             'password' => static::$password ?? Hash::make('password'),
             'user_type' => 'Supplier',
             'store_owner_info' => json_encode([
-                'tax_card_number'=>11,
-                'tax_registration_number'=>11
+                'tax_card_number'=>112233344,
+                'tax_registration_number'=>333666999
             ]),
         ]);
-        Store::create([
-           'owner_id'=>$supplier->id,
-           'store_name'=>'TuxStore' ,
-            'address'=>'TuxAddr',
-            'phone_number'=>111,
-            'type'=>'بقالة',
-            'valid'=>1,
-            'latitude'=>'30.05283352',
-            'longitude'=>'30.05283352',
+        Store::create( [
+            'id' => 2,
+            'owner_id' => $supplier->id,
+            'store_name' => 'تموين الحي العاشر',
+            'address' => '29VG+H6P, Al Hay Al Asher, Nasr City, Cairo Governorate 4442255',
+            'phone_number' => '0',
+            'type' => 'بقاله',
+            'valid' => 1,
+            'latitude' => '30.04518064',
+            'longitude' => '31.37614544',
+            'created_at' => $currentTimestamp,
+            'updated_at' => $currentTimestamp,
         ]);
         $supplier->assignRole('supplier');
+
 
 
     }
